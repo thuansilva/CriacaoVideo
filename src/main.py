7aa6f0b5df04596ed4c67b5dd368e4f3bebbc132
Thuan Silva <thuansilva14@gmail.com>
2025-02-22 02:41:24 -0300

    Adicionar Comando que aceita argumentos passados via linha de comando

--- src/main.py
+++ src/main.py
@@ -1,10 +1,16 @@
+import argparse
 from processar_video import video_com_banner,gerar_nome_arquivo,video_com_bordas_desfocadas
 from download_video import download_video_from_youtube
 
 if __name__ == "__main__":
+    # Passar URL
+    parser = argparse.ArgumentParser(description="Comando para passar uma URL")
+    parser.add_argument('--url', type=str, required=True, help="A URL a ser inserida no código")
+    args = parser.parse_args()
+
     nome_arquivo = gerar_nome_arquivo()
     localDownload = f"./downloads/reels_{nome_arquivo}.mp4"
-    video_url = 'https://www.youtube.com/shorts/51K1vMMB-tg'
+    video_url = args.url
 
     output_video = f"./video_final/reels_{nome_arquivo}.mp4"
 
